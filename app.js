fbab68f342bbf3fb7cd78fb57efdc3373852bfe8
DanMagicDev <dp.minski@gmail.com>
2022-05-10 18:08:53 +0300

    Last commit

--- app.js
+++ app.js
@@ -10,6 +10,38 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const worksMenu = document.querySelector('#works-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    worksMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    worksMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
 
 //  Close mobile Menu when clicking on a menu item
 const hideMobileMenu = () => {
@@ -23,5 +55,18 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 
+
+
+
+// Dark|Light mode
+    document.addEventListener("DOMContentLoaded", function() {
+    document.documentElement.setAttribute("data-theme", "light");
+    var themeSwitcher = document.getElementById("theme-switcher");
+    themeSwitcher.onclick = function() {
+      var currentTheme = document.documentElement.getAttribute("data-theme");
+      var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+document.documentElement.setAttribute("data-theme", switchToTheme);}
+
+});
 
 
